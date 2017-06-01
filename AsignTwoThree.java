701ed6e6d47989aab490be1d79f3d599cd50df44
ITSOK\anonm <dev1ender@gmail.com>
2017-06-01 15:20:33 +0530

    new final with output screanshot

--- AsignTwoThree.java
+++ AsignTwoThree.java
@@ -4,20 +4,25 @@ public class AsignTwoThree {
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
-		
+		System.out.println("Enter the value of a:");
 		int a = sc.nextInt();
+		System.out.println("Enter the value of b:");
 		int b = sc.nextInt();
+
+		System.out.println("Enter the value of c:");
 		int c =sc.nextInt();
 		
+	
+		//Comapring the value of a with b&c
 		if(a > b && a > c){
-			System.out.println(c);
+			System.out.println(a);
 		}
 		else if(b > a && b >c){
 			System.out.println(b);
 			
 		}
 		else 
-			System.out.println(a);
+			System.out.println(c);
 
 	}
 
